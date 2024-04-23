3640419e80af9aebde7727fddd43e0c95a9b6977
valentinacires <160196927+valentinacires@users.noreply.github.com>
2024-04-22 20:10:05 -0500

    fixed remote state

--- rds/main.tf
+++ rds/main.tf
@@ -10,3 +10,17 @@ data "tfe_outputs" "vpc" {
 output all {
     value - data.tfe_outputs.vpc.outputs 
 }
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "valentinacires"
+    workspaces = {
+      name = "vpc"
+    }
+  }
+}
+
+output all {
+    value = data.terraform_remote_state.vpc.outputs
+}
