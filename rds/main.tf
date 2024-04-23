755475ab307470cd8278b12b45b34fa0c0566a4e
valentinacires <160196927+valentinacires@users.noreply.github.com>
2024-04-22 20:28:40 -0500

    added instance

--- rds/main.tf
+++ rds/main.tf
@@ -17,3 +17,17 @@ resource "aws_db_subnet_group" "default" {
   name       = "terraform-cloud"
   subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
 }
+
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.default.name
+}
