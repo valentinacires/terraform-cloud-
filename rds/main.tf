93e42c93d3e5fe028fee388e64059633332a986a
valentinacires <160196927+valentinacires@users.noreply.github.com>
2024-04-22 21:05:15 -0500

    apply delete

--- rds/main.tf
+++ rds/main.tf
@@ -9,21 +9,3 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
-output all {
-    value = data.terraform_remote_state.vpc.outputs
-}
-  name       = "terraform-cloud"
- 
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  db_name              = "mydb"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-  db_subnet_group_name = aws_db_subnet_group.default.name
-}
-
