f3238794c44f4361023e94ad7eefde510ae19864
valentinacires <160196927+valentinacires@users.noreply.github.com>
2024-04-22 20:13:04 -0500

    remote reaply

--- rds/main.tf
+++ rds/main.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "vpc" {
   backend = "remote"
 
   config = {
-    organization = "Sadykov123"
+    organization = "valentinacires"
     workspaces = {
       name = "vpc"
     }
